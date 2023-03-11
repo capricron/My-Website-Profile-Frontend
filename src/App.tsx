e33253e796d1271a4b110c878139fda7ca5647c6
Cicak Kayang <eriscapricron@gmail.com>
2023-03-12 04:23:44 +0700

    setup react dan tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from "react";
+
+import { Outlet } from "react-router-dom";
+
 import './App.css';
 
-function App() {
+import Sidebar from "./partials/Sidebar";
+import Footer from "./partials/Footer";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {/* <Sidebar /> */}
+
+      <Outlet />
+      {/* <Footer /> */}
+    </>
+  )
 }
 
 export default App;
